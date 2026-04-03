e6e35102d7a9a7757630a39fd9f4ff8795acacc0
Nam <dinhhoainam260@gmail.com>
2026-04-03 19:12:29 +0700

    feat(dao) : Tạo xong SessionRegistrationDao vưới các phương thức insert,check và get

--- server/src/main/java/com/uet/BiddingApplication/DAO/Interface/ISessionRegistrationDAO.java
+++ server/src/main/java/com/uet/BiddingApplication/DAO/Interface/ISessionRegistrationDAO.java
@@ -1,11 +1,12 @@
 package com.uet.BiddingApplication.DAO.Interface;
 
 import com.uet.BiddingApplication.DTO.Response.AuctionCardDTO;
+import com.uet.BiddingApplication.Model.SessionRegistration;
 
 import java.util.List;
 
 public interface ISessionRegistrationDAO {
-    boolean registerBidder(String bidderId,String sessionId);
+    boolean registerBidder(SessionRegistration registration);
     boolean checkRegistration(String bidderId,String sessionId);
     boolean deleteRegistration(String bidderId,String sessionId);
     List<AuctionCardDTO> getRegisteredSessions(String bidderId);
--- server/src/test/java/com/uet/BiddingApplication/DAO/Impl/AuctionSessionDAOTest.java
+++ server/src/test/java/com/uet/BiddingApplication/DAO/Impl/AuctionSessionDAOTest.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-public class AuctionSessionTest {
+public class AuctionSessionDAOTest {
     @Disabled
     @Test
     public void testAuctionSession() {
