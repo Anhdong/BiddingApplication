77a34fbd1d1d94ae92e04aed55f0e3bac76e7050
Nam <dinhhoainam260@gmail.com>
2026-04-08 10:02:07 +0700

    feat(exception) : tạo class BusinessException

---
+++ common/src/main/java/com/uet/BiddingApplication/Exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.uet.BiddingApplication.Exception;
+
+public class BusinessException extends Exception {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
