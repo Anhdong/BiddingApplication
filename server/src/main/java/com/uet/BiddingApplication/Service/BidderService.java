14894be61eb47eaaea83a5078a5ad6d6ce4a381d
dekuhoccode <25021782@vnu.edu.vn>
2026-04-18 11:35:25 +0700

    hoàn thành cơ bản bidderService

--- server/src/main/java/com/uet/BiddingApplication/Service/BidderService.java
+++ server/src/main/java/com/uet/BiddingApplication/Service/BidderService.java
@@ -2,9 +2,10 @@ package com.uet.BiddingApplication.Service;
 
 import java.util.List;
 
+import com.uet.BiddingApplication.DAO.Impl.BidDAO;
+import com.uet.BiddingApplication.DAO.Impl.SessionRegistrationDAO;
 import com.uet.BiddingApplication.DTO.Response.AuctionCardDTO;
-import com.uet.BiddingApplication.DTO.Response.BidHistoryDTO;
-import com.uet.BiddingApplication.Model.AuctionSession;
+import com.uet.BiddingApplication.DTO.Response.BidderHistoryResponseDTO;
 
 public class BidderService {
 
@@ -40,24 +41,18 @@ public class BidderService {
         // TODO 2: Gọi phương thức lấy danh sách phiên đã đăng ký bằng ID của bidder
         // TODO 3: Trả về list danh sách đó (thay vì return null)
 
-        // Cú pháp gợi ý (chờ Tech Lead hoàn thiện):
-        // return SessionRegistrationDAO.getInstance().getRegisteredSessions(bidderId);
-
-        return null;
+        return SessionRegistrationDAO.getInstance().getRegisteredSessions(bidderId);
     }
 
     /**
      * Trả về lịch sử tất cả các lần vung tiền của Bidder này.
      */
-    public List<BidHistoryDTO> getBidderHistory(String bidderId){
+    public List<BidderHistoryResponseDTO> getBidderHistory(String bidderId){
         // TODO 1: Lấy instance của BidDAO (Do Tech Lead cung cấp)
         // TODO 2: Gọi phương thức lấy toàn bộ lịch sử vung tiền của user này
         // TODO 3: Nếu DAO trả về đúng kiểu List<BidHistoryDTO> thì return thẳng,
         //         nếu khác kiểu (ví dụ List<BidderHistoryResponseDTO>) thì cần viết vòng lặp để map/chuyển đổi dữ liệu.
 
-        // Cú pháp gợi ý (chờ Tech Lead hoàn thiện):
-        // return BidDAO.getInstance().getBidderHistory(bidderId);
-
-        return null;
+        return BidDAO.getInstance().getBidderHistory(bidderId);
     }
 }
