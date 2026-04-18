ec6f609942688122c8ecdaa8d2d27df723beebea
dekuhoccode <25021782@vnu.edu.vn>
2026-04-18 11:36:56 +0700

    Sửa lỗi AuctionViewMapper

--- common/src/main/java/com/uet/BiddingApplication/Utils/Mapper/AuctionViewMapper.java
+++ common/src/main/java/com/uet/BiddingApplication/Utils/Mapper/AuctionViewMapper.java
@@ -1,6 +1,7 @@
 package com.uet.BiddingApplication.Utils.Mapper;
 
 import com.uet.BiddingApplication.DTO.Response.AuctionCardDTO;
+import com.uet.BiddingApplication.DTO.Response.SessionInfoResponseDTO;
 import com.uet.BiddingApplication.Model.AuctionSession;
 import com.uet.BiddingApplication.Model.Item;
 
