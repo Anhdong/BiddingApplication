29a8df1a4badfeef394428baef2dbca3aa5060f6
Anhdong <mobundong@gmail.com>
2026-04-30 00:37:17 +0700

    feat: Add loading sidebar depend on role

--- client/src/main/java/com/uet/BiddingApplication/controller/MainViewController.java
+++ client/src/main/java/com/uet/BiddingApplication/controller/MainViewController.java
@@ -1,28 +1,63 @@
 package com.uet.BiddingApplication.controller;
 
+import com.uet.BiddingApplication.Enum.RoleType;
+import com.uet.BiddingApplication.Exception.BusinessException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainViewController {
+public class MainViewController implements Initializable {
 
     @FXML
     private StackPane SidebarSlot;
     @FXML
     private StackPane ContentSlot;
 
-    @FXML
-    public void initialize() {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setupUserInterface(RoleType.SELLER);
+        setContentSlot("/app/fxml/BidderView/AuctionView.fxml");
+
+    }
+
+    public void setupUserInterface(RoleType role) {
+        //Check role
+        String sidebarPath = null;
+        switch (role){
+            case RoleType.BIDDER:
+                sidebarPath = "/app/fxml/SidebarView/BidderSidebar.fxml";
+                break;
+            case RoleType.SELLER:
+                sidebarPath = "/app/fxml/SidebarView/SellerSidebar.fxml";
+                break;
+            case RoleType.ADMIN:
+                sidebarPath = "/app/fxml/SidebarView/AdminSidebar.fxml";
+                break;
+            default:
+                throw new RuntimeException("Role Invalid");
+        }
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/fxml/SidebarView/BidderSidebar.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(sidebarPath));
             Parent sidebar = loader.load();
-
             SidebarSlot.getChildren().setAll(sidebar);
+        } catch (Exception e) {e.printStackTrace();}
 
-        } catch (IOException e){System.err.println("[MainViewController] Không tải đc sidebar" );}
+    }
+
+    public void setContentSlot(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent content = loader.load();
+
+            ContentSlot.getChildren().setAll(content);
+
+        } catch (IOException e){System.err.println("[MainViewController] Không tải  được Tab." );}
     }
 
 }
