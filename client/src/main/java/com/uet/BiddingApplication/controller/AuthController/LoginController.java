eab28841a458dcf1111886da5b066d4cb787e691
Anhdong <mobundong@gmail.com>
2026-04-30 10:53:57 +0700

    fix: Add Objects.requireNonNull to AuthController

--- client/src/main/java/com/uet/BiddingApplication/controller/AuthController/LoginController.java
+++ client/src/main/java/com/uet/BiddingApplication/controller/AuthController/LoginController.java
@@ -4,14 +4,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;;
+import javafx.scene.input.MouseEvent;
+
+import java.util.Objects;
 
 public class LoginController {
 
     public void SwitchToRegister(MouseEvent event) {
         Parent registerRoot = null;
         try {
-            registerRoot = FXMLLoader.load(getClass().getResource("/app/fxml/AuthView/RegisterView.fxml"));
+            registerRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/app/fxml/AuthView/RegisterView.fxml")));
         } catch (Exception e) {e.printStackTrace();}
 
         Scene currentScene = ((Node) event.getSource()).getScene();
--- client/src/main/java/com/uet/BiddingApplication/controller/AuthController/RegisterController.java
+++ client/src/main/java/com/uet/BiddingApplication/controller/AuthController/RegisterController.java
@@ -6,12 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 
+import java.util.Objects;
+
 public class RegisterController {
 
     public void SwitchToLogin(MouseEvent event) {
     Parent loginRoot = null;
     try {
-        loginRoot = FXMLLoader.load(getClass().getResource("/app/fxml/AuthView/LoginView.fxml"));
+        loginRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/app/fxml/AuthView/LoginView.fxml")));
     } catch (Exception e) {e.printStackTrace();}
 
     Scene currentScene = ((Node) event.getSource()).getScene();
