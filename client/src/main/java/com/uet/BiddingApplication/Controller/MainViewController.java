9d8cc78fefe44412fa0409423ea29186928147dc
Anhdong <mobundong@gmail.com>
2026-04-30 16:26:43 +0700

    feat: Add ViewCache to MainViewController

--- client/src/main/java/com/uet/BiddingApplication/Controller/MainViewController.java
+++ client/src/main/java/com/uet/BiddingApplication/Controller/MainViewController.java
@@ -5,59 +5,61 @@ import com.uet.BiddingApplication.Enum.RoleType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.EnumMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
 
-    @FXML
-    private StackPane SidebarSlot;
-    @FXML
-    private StackPane ContentSlot;
+    @FXML private StackPane SidebarSlot;
+    @FXML private StackPane ContentSlot;
+
+    // View Cache
+    private final Map<ViewPath, Node> viewCache = new EnumMap<>(ViewPath.class);
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setupUserInterface(RoleType.SELLER);
-        setContentSlot(ViewPath.BIDDER_AUCTION);
-
+        loadView(ViewPath.BIDDER_AUCTION);
     }
 
     public void setupUserInterface(RoleType role) {
         //Check role
-        ViewPath sidebarPath = null;
-        switch (role){
-            case RoleType.BIDDER:
-                sidebarPath = ViewPath.BIDDER_SIDEBAR;
-                break;
-            case RoleType.SELLER:
-                sidebarPath = ViewPath.SELLER_SIDEBAR;
-                break;
-            case RoleType.ADMIN:
-                sidebarPath = ViewPath.ADMIN_SIDEBAR;
-                break;
-            default:
-                throw new RuntimeException("Role Invalid");
-        }
-        try {
+        ViewPath sidebarPath = switch (role) {
+            case RoleType.BIDDER -> ViewPath.BIDDER_SIDEBAR;
+            case RoleType.SELLER -> ViewPath.SELLER_SIDEBAR;
+            case RoleType.ADMIN -> ViewPath.ADMIN_SIDEBAR;
+        };
+
+        try { //set Sidebar
             FXMLLoader loader = new FXMLLoader(getClass().getResource(sidebarPath.getPath()));
             Parent sidebar = loader.load();
             SidebarSlot.getChildren().setAll(sidebar);
-        } catch (Exception e) {e.printStackTrace();}
+        } catch (Exception e) {System.err.println("[MainViewController] Thiết lập Sidebar không thành công.");}
 
     }
 
-    public void setContentSlot(ViewPath fxmlPath) {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath.getPath()));
-            Parent content = loader.load();
 
-            ContentSlot.getChildren().setAll(content);
-
-        } catch (IOException e){System.err.println("[MainViewController] Không tải  được Tab." );}
+    public void loadView(ViewPath target) {
+        // computeIfAbsent: Nếu có trong cache thì lấy, chưa có thì mới chạy đoạn code load FXML
+        Node view = viewCache.computeIfAbsent(target, pathEnum -> {
+            try {
+                System.out.println("Đang load mới: " + pathEnum.getPath());
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(pathEnum.getPath()));
+                return loader.load();
+            } catch (IOException e) {
+                System.out.print("[MainViewController] Không thể thiết lập giao diện nội dung.");
+                return null;
+            }
+        });
+        if (view != null) {
+            ContentSlot.getChildren().setAll(view);
+        }
     }
-
 }
