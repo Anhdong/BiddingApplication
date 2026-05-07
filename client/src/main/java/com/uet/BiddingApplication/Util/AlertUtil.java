062f2236c82e57b225865a07317ab0a4d71ea42c
Anhdong <mobundong@gmail.com>
2026-05-07 23:18:47 +0700

    feat: Add AlertUtil

---
+++ client/src/main/java/com/uet/BiddingApplication/Util/AlertUtil.java
@@ -0,0 +1,17 @@
+package com.uet.BiddingApplication.Util;
+
+import javafx.scene.control.Alert;
+
+public class AlertUtil {
+    public static void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR); // Thay đổi Type tùy ý
+        alert.setTitle(title);
+        alert.setHeaderText(null); // Để null nếu không muốn có dòng tiêu đề phụ
+        alert.setContentText(content);
+        alert.showAndWait(); // Hiển thị và dừng các tương tác khác cho đến khi nhấn OK
+    }
+
+    public static void showAlert(String content){
+        showAlert("Error!",content);
+    }
+}
