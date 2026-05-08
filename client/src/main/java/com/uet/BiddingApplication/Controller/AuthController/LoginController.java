222167db96efe15267c0339ed0ab6e789a05d5b0
Anhdong <mobundong@gmail.com>
2026-05-08 22:01:30 +0700

    feat: Finish AuthController Logic

--- client/src/main/java/com/uet/BiddingApplication/Controller/AuthController/LoginController.java
+++ client/src/main/java/com/uet/BiddingApplication/Controller/AuthController/LoginController.java
@@ -1,40 +1,67 @@
 package com.uet.BiddingApplication.Controller.AuthController;
 
+import com.uet.BiddingApplication.DTO.Packet.RequestPacket;
+import com.uet.BiddingApplication.DTO.Packet.ResponsePacket;
+import com.uet.BiddingApplication.DTO.Request.AuthRequestDTO;
+import com.uet.BiddingApplication.DTO.Response.AuthResponseDTO;
+import com.uet.BiddingApplication.Enum.ActionType;
 import com.uet.BiddingApplication.Enum.ViewPath;
+import com.uet.BiddingApplication.Session.ClientSession;
+import com.uet.BiddingApplication.Session.ResponseDispatcher;
+import com.uet.BiddingApplication.Session.ServerConnection;
 import com.uet.BiddingApplication.Util.AlertUtil;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import static com.uet.BiddingApplication.BiddingApplication.primaryStage;
 
-public class LoginController {
-
+public class LoginController implements Initializable {
 
     @FXML private TextField txtUsername;
     @FXML private PasswordField txtPassword;
 
-    public void switchToRegister() {
-        Parent registerRoot = null;
-        try {
-            registerRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(ViewPath.REGISTER.getPath())));
-        } catch (Exception e) {System.out.println("[LoginController] Cannot load RegisterView");}
+    // Định nghĩa callback thành một biến để có thể unsubscribe chính xác
+    private final Consumer<ResponsePacket<?>> loginCallback = this::handleLoginResponse;
 
-        Scene currentScene = primaryStage.getScene();
-        currentScene.setRoot(registerRoot);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // 1. Đăng ký lắng nghe sự kiện trả về cho luồng Đăng nhập
+        ResponseDispatcher.getInstance().subscribe(ActionType.LOGIN, loginCallback);
     }
 
     //Main func
     @FXML
     private void handleLogin(){
-        if(validateInput()){
+        //Check điều kiện
+        if(!validateInput()) return;
 
-        }
+        // Lấy thông tin
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        //Bọc vô DTO
+        AuthRequestDTO authDTO = new AuthRequestDTO();
+        authDTO.setUsername(username);
+        authDTO.setPassword(password);
+
+        RequestPacket<AuthRequestDTO> request = new RequestPacket<>();
+        request.setAction(ActionType.LOGIN);
+        request.setPayload(authDTO);
+
+        //Gửi request
+        ServerConnection.getInstance().sendRequest(request);
+
+
     }
 
     private boolean validateInput(){
@@ -49,9 +76,43 @@ public class LoginController {
 
         return true;}
 
-    private void sendLoginRequest(){}
+    private void handleLoginResponse(ResponsePacket<?> response) {
+        if (response.getStatusCode() == 200) { // Thành công
+            AuthResponseDTO authData = (AuthResponseDTO) response.getPayload();
 
-    private void handleLoginResponse(){}
+            // Cập nhật phiên đăng nhập cục bộ
+            ClientSession.getInstance().updateLocalSession(authData.getUserProfile(), authData.getToken());
+            System.out.println("Đăng nhập thành công! Xin chào " + authData.getUserProfile().getUsername());
 
-    private void switchToMain(){}
+            //Chuyen ve Main
+            switchToMain();
+        } else {
+            System.err.println("Đăng nhập thất bại: " + response.getMessage());
+        }
+    }
+
+    public void switchToRegister() {
+        //Unsubcribe khi chuyển
+        ResponseDispatcher.getInstance().unsubscribe(ActionType.LOGIN, loginCallback);
+
+        Parent registerRoot = null;
+        try {
+            registerRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(ViewPath.REGISTER.getPath())));
+        } catch (Exception e) {System.out.println("[LoginController] Cannot load RegisterView");}
+
+        Scene currentScene = primaryStage.getScene();
+        currentScene.setRoot(registerRoot);
+    }
+    private void switchToMain(){
+        //Unsubcribe khi chuyển
+        ResponseDispatcher.getInstance().unsubscribe(ActionType.LOGIN, loginCallback);
+
+        Parent mainRoot = null;
+        try {
+            mainRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(ViewPath.MAIN.getPath())));
+        } catch (Exception e) {System.out.println("[LoginController] Cannot load Main");}
+
+        Scene currentScene = primaryStage.getScene();
+        currentScene.setRoot(mainRoot);
+    }
 }
